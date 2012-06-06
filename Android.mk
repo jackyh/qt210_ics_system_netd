226c0ec6ef473b74a448b199fe048a7513c5af4e
chengjie-he <chengjie.he@linaro.org>
2012-06-06 11:21:59 +0800

    initialized

---
+++ Android.mk
@@ -0,0 +1,70 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=                                      \
+                  BandwidthController.cpp              \
+                  CommandListener.cpp                  \
+                  DnsProxyListener.cpp                 \
+                  NatController.cpp                    \
+                  NetdCommand.cpp                      \
+                  NetlinkHandler.cpp                   \
+                  NetlinkManager.cpp                   \
+                  PanController.cpp                    \
+                  PppController.cpp                    \
+                  ResolverController.cpp               \
+                  SecondaryTableController.cpp         \
+                  TetherController.cpp                 \
+                  ThrottleController.cpp               \
+                  logwrapper.c                         \
+                  main.cpp                             \
+
+ifeq ($(BOARD_WIFI_VENDOR), realtek)
+    LOCAL_SRC_FILES += ../../hardware/realtek/wlan/netd/SoftapController_realtek.cpp
+else
+    LOCAL_SRC_FILES += SoftapController.cpp
+endif
+
+LOCAL_MODULE:= netd
+
+LOCAL_C_INCLUDES := $(KERNEL_HEADERS) \
+                    $(LOCAL_PATH)/../bluetooth/bluedroid/include \
+                    $(LOCAL_PATH)/../bluetooth/bluez-clean-headers \
+                    external/openssl/include \
+                    external/stlport/stlport \
+                    bionic \
+                    $(call include-path-for, libhardware_legacy)/hardware_legacy
+
+LOCAL_CFLAGS :=
+
+ifeq ($(BOARD_WIFI_VENDOR), realtek)
+    LOCAL_STATIC_LIBRARIES := libhostapd_client
+endif
+
+LOCAL_SHARED_LIBRARIES := libstlport libsysutils libcutils libnetutils \
+                          libcrypto libhardware_legacy
+
+ifneq ($(BOARD_HOSTAPD_DRIVER),)
+  LOCAL_CFLAGS += -DHAVE_HOSTAPD
+endif
+
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+  LOCAL_SHARED_LIBRARIES := $(LOCAL_SHARED_LIBRARIES) libbluedroid
+  LOCAL_CFLAGS := $(LOCAL_CFLAGS) -DHAVE_BLUETOOTH
+endif
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:=          \
+                  ndc.c \
+
+LOCAL_MODULE:= ndc
+
+LOCAL_C_INCLUDES := $(KERNEL_HEADERS)
+
+LOCAL_CFLAGS := 
+
+LOCAL_SHARED_LIBRARIES := libcutils
+
+include $(BUILD_EXECUTABLE)
